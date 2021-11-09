58f5545f05bc3096a49a4c7b07e74a59690b3f93
Henry <henry@Stefanias-MacBook-Pro.local>
2021-11-09 14:22:51 +0000

    updated mapApi

--- application/controllers/MapApi.php
+++ application/controllers/MapApi.php
@@ -39,15 +39,15 @@ class MapApi extends REST_Controller {
         
         if($flag == "r"){
         
-            $this->db->select('i.invoice_no,r.res_code,r.gps_lat,r.gps_long,i.payment_status,concat(prop.firstname," ",prop.lastname) as owner_name,prop.primary_contact as owner_phoneno,a.name as area_council,t.town as town');
-            $this->db->from('residence as r');
-            $this->db->join('residence_to_owner as ro','r.id = ro.property_id','left');
+            $this->db->select('i.invoice_no,b.buis_prop_code,b.gps_lat,b.gps_long,i.payment_status,concat(prop.firstname," ",prop.lastname) as owner_name,prop.primary_contact as owner_phoneno,a.name as area_council,t.town as town');
+            $this->db->from('buisness_property as b');
+            $this->db->join('buis_prop_to_owner as ro','b.id = ro.property_id','left');
             $this->db->join('property_owner as prop','ro.owner_id = prop.id','left');
-            $this->db->join('town as t','r.town = t.id','left');
-            $this->db->join('area_council as a','r.area_council = a.id','left');
-            $this->db->join("vw_invoice as i","i.property_id = r.id");
+            $this->db->join('town as t','b.town = t.id','left');
+            $this->db->join('area_council as a','b.area_council = a.id','left');
+            $this->db->join("vw_invoice as i","i.property_id = b.id");
             $this->db->join("revenue_product as rp","rp.id = i.product_id");
-            $this->db->where("r.gps_lat <> 0 AND r.gps_long <> 0 AND rp.target = 1");
+            $this->db->where("b.gps_lat <> 0 AND b.gps_long <> 0 AND rp.target = 1 AND b.category = 13");
             ($payment_where) ? $this->db->where_in('i.payment_status', $payment_where) : NULL;
             ($area_council) ? $this->db->where('a.id', $area_council) : NULL;
             ($town) ? $this->db->where('t.id', $town) : NULL;
